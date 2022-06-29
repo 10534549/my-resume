d2f12f94665c140788a0becee461c6d84b5c4df3
Sravya Lavu <58265969+10534549@users.noreply.github.com>
2022-06-29 15:54:30 +0530

    Add files via upload

---
+++ resume/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+
+        }
+    })
+});
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+
